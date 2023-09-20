b7ce8bbe70ca38b78e86ec1a704167386f6d543c
PHAGUN JAIN <PHAGUNJAIN@protonmail.com>
2023-09-20 16:13:45 +0000

    java refresher

---
+++ java/Main.java
@@ -0,0 +1,10 @@
+import java.io.*;
+import matrix.*;
+
+class Main{
+    public static void main(String[] args){
+        System.out.println("Hello World!");
+        vector_vector_multiplication obj = new vector_vector_multiplication();
+        obj.multiply();
+    }
+}
---
+++ java/matrix/vector_vector_multiplication.java
@@ -0,0 +1,9 @@
+package matrix;
+
+public class vector_vector_multiplication {
+
+    public void multiply(){
+        System.out.println("called vector_vector multiplication");
+    }
+    
+}
